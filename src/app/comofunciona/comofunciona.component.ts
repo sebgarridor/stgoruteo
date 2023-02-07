64555ebe3bf04a2a1336560e2836e56ba997fad5
Sebastián Garrido <sebgarridor@gmail.com>
2023-02-07 15:51:23 -0300

    creating new component

---
+++ src/app/comofunciona/comofunciona.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comofunciona',
+  templateUrl: './comofunciona.component.html',
+  styleUrls: ['./comofunciona.component.scss']
+})
+export class ComofuncionaComponent {
+
+}
