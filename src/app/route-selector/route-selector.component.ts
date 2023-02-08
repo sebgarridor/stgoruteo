0661fe3a5d31914e543d82c13721f5bcb819db76
Sebastián Garrido <sebgarridor@gmail.com>
2023-02-08 16:26:47 -0300

    adding card to each route

--- src/app/route-selector/route-selector.component.ts
+++ src/app/route-selector/route-selector.component.ts
@@ -18,6 +18,8 @@ class Route {
   }
 }
 
+
+
 @Component({
   selector: 'app-route-selector',
   templateUrl: './route-selector.component.html',
@@ -28,7 +30,7 @@ export class RouteSelectorComponent { routes: Route[] = [
   new Route("Route 2", 20, 200, 2000, "https://image2.jpg", "norte"),
   new Route("Route 3", 30, 300, 3000, "https://image3.jpg", "oriente"),
   new Route("Route 4", 40, 400, 4000, "https://image4.jpg", "oriente"),
-  new Route("Route 5", 50, 500, 5000, "https://image5.jpg", "poniente"),
+  new Route("Cuesta Barriga", 50, 500, 5000, "https://i.imgur.com/jT9z5G6.png", "poniente"),
   new Route("Route 6", 60, 600, 6000, "https://image6.jpg", "poniente"),
   new Route("Route 7", 70, 700, 7000, "https://image7.jpg", "sur"),
   new Route("Route 8", 80, 800, 8000, "https://image8.jpg", "sur"),
